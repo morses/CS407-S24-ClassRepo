f7a08e61cd3e1cde71f15865434445d0d052a855
Scot Morse <morses@wou.edu>
2024-04-03 15:52:22 -0700

    starting the sveltekit example

---
+++ HelloSvelteKit/src/routes/assignment1/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+let name = ['John', 'Doe', 'Jane', 'Doe', 'John', 'Smith', 'Jane', 'Smith'];
+import {confetti} from '@neoconfetti/svelte';
+import {WebGLRenderer} from 'three';
+</script>
+
+<div>
+    <h1 use:confetti>Assignment 1</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, possimus voluptatem asperiores reiciendis modi quasi in necessitatibus numquam perspiciatis quam atque dicta itaque quisquam a deserunt amet labore aliquid officiis.</p>
+</div>
+<div>
+    <h2>Names</h2>
+    <ul>
+        {#each name as n}
+            <li>{n}</li>
+        {/each}
+    </ul>
+</div>
+<canvas>
+
+</canvas>
+
+<style>
+
+</style>
